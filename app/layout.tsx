f74b63682d0406fa5e61b1334b569f2daa77f542
RoboticsAAA20Group <hans.preinfalk.davila@gmail.com>
2025-09-25 19:54:45 -0600

    added clerk auth

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,8 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { ThemeProvider } from "@/components/theme-provider";
-import { ConvexClientProvider } from "./ConvexClientProvider";
+import { ClerkProvider } from "@clerk/nextjs";
+import ConvexClientProvider from "@/components/convex-client-provider";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -29,16 +30,19 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        <ThemeProvider
-          attribute="class"
-          defaultTheme="dark"
-          enableSystem
-          disableTransitionOnChange
-        >
+        <ClerkProvider>
           <ConvexClientProvider>
-            {children}
+            <ThemeProvider
+              attribute="class"
+              defaultTheme="dark"
+              enableSystem
+              disableTransitionOnChange
+            >
+
+              {children}
+            </ThemeProvider>
           </ConvexClientProvider>
-        </ThemeProvider>
+        </ClerkProvider>
       </body>
     </html>
   );
