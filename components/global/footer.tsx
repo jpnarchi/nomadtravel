350273369ea70b3ec403b4fb001d565785c4dc8d
jpnarchi <142521385+jpnarchi@users.noreply.github.com>
2025-11-01 11:47:28 -0600

    Cambios

--- components/global/footer.tsx
+++ components/global/footer.tsx
@@ -16,7 +16,7 @@ export function Footer() {
         <footer className="py-6">
             <div className="mx-auto px-6">
                 <div className="flex flex-wrap justify-between gap-6">
-                    <span className="text-muted-foreground order-last block text-center text-sm md:order-first">© {new Date().getFullYear()} I Love Presentations, All rights reserved</span>
+                    <span className="text-muted-foreground order-last block text-center text-sm md:order-first">© {new Date().getFullYear()} iLovePresentations.ai, All rights reserved</span>
                     <div className="order-first flex flex-wrap justify-center gap-6 text-sm md:order-last">
                         {links.map((link, index) => (
                             <Link
