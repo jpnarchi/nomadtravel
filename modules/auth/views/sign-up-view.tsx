2c392a0f71a54d94f67a91ca6b4930d308fd9145
jpnarchi <142521385+jpnarchi@users.noreply.github.com>
2025-11-25 10:17:01 -0600

    Fix de boton

--- modules/auth/views/sign-up-view.tsx
+++ modules/auth/views/sign-up-view.tsx
@@ -7,7 +7,7 @@ export const SignUpView = () => {
 
         >
             {/* Columna izquierda - Formulario de Sign Up */}
-            <div className="absolute left-0 top-0 w-full h-full md:w-1/2 flex items-center justify-center p-8 z-10 pt-10 lg:pt-30 -ml-3 md:ml-0">
+            <div className="absolute left-0 top-0 w-full h-full md:w-1/2 flex items-center justify-center p-4 md:p-8 z-10 pt-10 lg:pt-30 md:ml-0">
                 <div className="w-full max-w-md space-y-4">
                     <h1 className="text-4xl lg:text-5xl font-semibold text-black -mb-2 ml-4">Sign up</h1>
                     <SignUp
@@ -63,6 +63,11 @@ export const SignUpView = () => {
                                     color: "white",
                                     fontWeight: "500",
                                     fontSize: "1rem",
+                                    whiteSpace: "nowrap",
+                                    overflow: "visible",
+                                    textOverflow: "clip",
+                                    width: "auto",
+                                    flexGrow: 1,
                                 },
                                 socialButtonsBlockButtonText__google: {
                                     fontSize: "1rem",
